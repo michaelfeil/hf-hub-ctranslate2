1406b9a1af0c0abd791843fbc023270cd3b959f4
Michael Feil <michael.feil@michaelfeil.eu>
2023-05-02 01:52:57 +0200

    release 1.0.2

--- hf_hub_ctranslate2/__init__.py
+++ hf_hub_ctranslate2/__init__.py
@@ -3,4 +3,4 @@
 __all__ = ["__version__", "TranslatorCT2fromHfHub", "GeneratorCT2fromHfHub"]
 from hf_hub_ctranslate2.translate import TranslatorCT2fromHfHub, GeneratorCT2fromHfHub
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
