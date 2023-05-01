58664835c0b2cdb4cb7af79933633bc1d3264307
Michael Feil <michael.feil@michaelfeil.eu>
2023-05-01 23:41:00 +0200

    improve coverage

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,6 +1,6 @@
 from hf_hub_ctranslate2 import TranslatorCT2fromHfHub, GeneratorCT2fromHfHub
-
-
+from hf_hub_ctranslate2._private.utils import download_model
+from transformers import AutoTokenizer
 def test_translator(model_name="michaelfeil/ct2fast-flan-alpaca-base"):
     model = TranslatorCT2fromHfHub(
         model_name_or_path=model_name, device="cpu", compute_type="int8"
@@ -30,6 +30,19 @@ def test_generator(model_name="michaelfeil/ct2fast-pythia-160m"):
     for o in outputs:
         assert isinstance(o, str)
 
+def test_generator_single(model_name="michaelfeil/ct2fast-pythia-160m"):
+    model_path = download_model(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    
+    model = GeneratorCT2fromHfHub(
+        model_name_or_path=model_path, device="cpu", compute_type="int8", tokenizer=tokenizer
+    )
+
+    outputs = model.generate(
+        "How do you call a fast Flan-ingo?"
+    )
+    assert isinstance(outputs, str)
+    assert "flan" in outputs.lower()
 
 if __name__ == "__main__":
     test_generator()
