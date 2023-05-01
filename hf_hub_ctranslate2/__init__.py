1871f1678e96e7f4fbd5e11c1e61c30a9cb04d76
Michael Feil <michael.feil@michaelfeil.eu>
2023-05-02 00:15:50 +0200

    remove files

--- hf_hub_ctranslate2/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Metric for clustering."""
-__all__ = ["__version__", "TranslatorCT2fromHfHub", "GeneratorCT2fromHfHub"]
-from hf_hub_ctranslate2.translate import TranslatorCT2fromHfHub, GeneratorCT2fromHfHub
-
-__version__ = "0.0.1"
