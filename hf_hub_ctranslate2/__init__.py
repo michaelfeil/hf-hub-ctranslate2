c36d715e5b9ab85fc844fb4892f52a1f444ef25e
Michael Feil <michael.feil@michaelfeil.eu>
2023-05-19 00:52:39 +0200

    update new version

--- hf_hub_ctranslate2/__init__.py
+++ hf_hub_ctranslate2/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Compatability between Huggingface and Ctranslate2."""
-__all__ = ["__version__", "TranslatorCT2fromHfHub", "GeneratorCT2fromHfHub", "MultiLingualTranslatorCT2fromHfHub", "_private"]
+__all__ = ["__version__", "TranslatorCT2fromHfHub", "GeneratorCT2fromHfHub", "MultiLingualTranslatorCT2fromHfHub"]
 from hf_hub_ctranslate2.translate import TranslatorCT2fromHfHub, GeneratorCT2fromHfHub, MultiLingualTranslatorCT2fromHfHub
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
