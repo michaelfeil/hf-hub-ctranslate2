696b6d47f263456c6f21dff3e504aa06bf187ac4
Michael Feil <michael.feil@michaelfeil.eu>
2023-05-20 01:56:40 +0200

    new version

--- hf_hub_ctranslate2/__init__.py
+++ hf_hub_ctranslate2/__init__.py
@@ -3,4 +3,4 @@
 # __all__ = ["__version__", "TranslatorCT2fromHfHub", "GeneratorCT2fromHfHub", "MultiLingualTranslatorCT2fromHfHub", "_private"]
 from hf_hub_ctranslate2.translate import TranslatorCT2fromHfHub, GeneratorCT2fromHfHub, MultiLingualTranslatorCT2fromHfHub
 from hf_hub_ctranslate2.util import _download_model
-__version__ = "2.0.6"
+__version__ = "2.0.7"
